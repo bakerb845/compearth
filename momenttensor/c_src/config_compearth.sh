011eff753657a2dd14abfe34f350f87239008138
jeffrey leifer <jeffreyleifer@isti.com>
2018-09-21 11:29:52 -0400

    fixing xonfig

--- momenttensor/c_src/config_compearth.sh
+++ momenttensor/c_src/config_compearth.sh
@@ -3,6 +3,4 @@
 -DCMAKE_INSTALL_PREFIX=./ \
 -DCMAKE_C_FLAGS="-g3 -O2 -fopenmp" \
 -DCOMPEARTH_USE_MKL=TRUE \
--DMKL_LIBRARY="/opt/intel/mkl/lib/intel64_lin/libmkl_intel_lp64.so;/opt/intel/mkl/lib/intel64_lin/libmkl_sequential.so;/opt/intel/mkl/lib/intel64_lin/libmkl_core.so" \
--DMKL_INCLUDE_DIR=/opt/intel/mkl/include \
 -DCOMPEARTH_BUILD_PYTHON_MODULE=ON
